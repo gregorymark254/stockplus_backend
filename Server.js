aa88e4727783eb13804bb87d48e7925a56698926
gregorymark254 <gregorymark254@gmail.com>
2024-05-15 23:07:19 +0300

    stock plus

--- Server.js
+++ Server.js
@@ -4,11 +4,11 @@ const cors = require('cors');
 const corsOption = require('./Db/corsOptions');
 require('dotenv').config();
 const { sqlconnect } = require('./Db/dbConfig');
-const swaggerJsDoc = require('swagger-jsdoc')
-const swaggerUI = require('swagger-ui-express')
+const options = require('./Swagger')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 const users = require('./Routes/users')
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-// const options = require('./Swagger')
 
 // connection to mysqldatabase
 // sqlconnect();
@@ -41,39 +41,9 @@ app.use(cors(corsOption));
 app.get('/', (req, res) => {
     res.json({ Message: 'Stock Plus Backend Server.' });
 });
-const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Library API",
-        version: "1.0.0",
-        description: "A simple Express Library API",
-        termsOfService: "http://example.com/terms/",
-        contact: {
-          name: "API Support",
-          url: "http://www.exmaple.com/support",
-          email: "support@example.com",
-        },
-      },
-      servers: [
-        {
-          url: "https://nodejs-swagger-api.vercel.app/",
-          description: "My API Documentation",
-        },
-      ],
-    },
-    // This is to call all the file
-    apis: ["src/**/*.js"],
-  };
-  
-  const specs = swaggerJsDoc(options);
-  // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-  
-  app.use(
-    "/api-docs",
-    swaggerUI.serve,
-    swaggerUI.setup(specs, { customCssUrl: CSS_URL })
-  );
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL }))
+app.use("/api", users); // users
 
 // Connetion to the server
 const PORT = process.env.PORT2;
