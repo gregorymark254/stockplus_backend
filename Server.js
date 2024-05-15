7f87d850723d5e9c74a7d040d58cb8c537e30daa
gregorymark254 <gregorymark254@gmail.com>
2024-05-15 23:47:29 +0300

    stockplus1

--- Server.js
+++ Server.js
@@ -3,137 +3,47 @@ const app = express();
 const cors = require('cors');
 const corsOption = require('./Db/corsOptions');
 require('dotenv').config();
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
 const { sqlconnect } = require('./Db/dbConfig');
-const swaggerJsDoc = require('swagger-jsdoc')
-const swaggerUI = require('swagger-ui-express')
+const options = require('./Swagger')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 const users = require('./Routes/users')
 
+// connection to mysqldatabase
+sqlconnect();
 
-// CDN CSS
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOption));
 
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+// routes
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Get homepage message
+ *     description: Retrieve the message displayed on the homepage of the Stock Plus Backend Server.
+ *     responses:
+ *       200:
+ *         description: A JSON object containing the homepage message.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 Message:
+ *                   type: string
+ *                   description: The message displayed on the homepage.
+ *                   example: Stock Plus Backend Server.
+ */
+// app.get('/', (req, res) => {
+//     res.json({ Message: 'Stock Plus Backend Server.' });
+// });
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use("/api", users); // users
 
-
-app.use(bodyParser.json()); // to use body object in requests
+// Connetion to the server
 const PORT = process.env.PORT2;
-
-app.use(morgan("dev"));
-app.use(cors());
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-      termsOfService: "http://example.com/terms/",
-      contact: {
-        name: "API Support",
-        url: "http://www.exmaple.com/support",
-        email: "support@example.com",
-      },
-    },
-    servers: [
-      {
-        url: "https://nodejs-swagger-api.vercel.app/",
-        description: "My API Documentation",
-      },
-    ],
-  },
-  // This is to call all the file
-  apis: ["src/**/*.js"],
-};
-
-const specs = swaggerJsDoc(options);
-// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-
-app.use(
-  "/api-docs",
-  swaggerUI.serve,
-  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
-);
-
-// Here we are calling the basic html
-// Use the router from the hello.js file
-// app.use("/", helloRouter);
-// // Use the router from the post.js file
-// app.use("/posts", postRouter);
-
-app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
-
-// import express from "express";
-// import cors from "cors";
-// import morgan from "morgan";
-// import dotenv from "dotenv";
-// import bodyParser from "body-parser";
-// import swaggerUI from "swagger-ui-express";
-// import swaggerJsDoc from "swagger-jsdoc";
-
-// // Import the router from the hello.js file
-// import postRouter from "./src/Routes/posts.js";
-// import helloRouter from "./src/hello.js";
-
-// //? CDN CSS
-// const CSS_URL =
-//   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
-// const app = express();
-
-// app.use(bodyParser.json()); // to use body object in requests
-// const PORT = process.env.PORT || 2000;
-// dotenv.config();
-
-// app.use(morgan("dev"));
-// app.use(cors());
-
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Library API",
-//       version: "1.0.0",
-//       description: "A simple Express Library API",
-//       termsOfService: "http://example.com/terms/",
-//       contact: {
-//         name: "API Support",
-//         url: "http://www.exmaple.com/support",
-//         email: "support@example.com",
-//       },
-//     },
-//     servers: [
-//       {
-//         url: "http://localhost:2000",
-//         description: "My API Documentation",
-//       },
-//     ],
-//   },
-//   // This is to call all the file
-//   apis: ["src/**/*.js"],
-// };
-
-// const specs = swaggerJsDoc(options);
-// // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-
-// // custome css added!
-// app.use("/public", express.static("public/swagger.css"));
-// app.use(
-//   "/api-docs",
-//   swaggerUI.serve,
-//   swaggerUI.setup(specs, { customCssUrl: "/public/swagger.css" && CSS_URL })
-// );
-
-// // Here we are calling the basic html
-// // Use the router from the hello.js file
-
-// //* All the routes goes here!
-// app.use("/", helloRouter);
-
-// // Use the router from the post.js file
-// app.use("/posts", postRouter);
-
-// //? This is for running the app
-// app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
