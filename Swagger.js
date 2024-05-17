110e7730290723d5f616f9d7dabf6afa3099c23d
gregorymark254 <gregorymark254@gmail.com>
2024-05-17 21:45:13 +0300

    stock

--- Swagger.js
+++ Swagger.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const swaggerJSDoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
 const options = {
     definition: {
@@ -50,6 +51,11 @@ const options = {
 };
 
 const swaggerSpec = swaggerJSDoc(options)
-router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, 
+    {
+        customCss:'.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+        customCssUrl: CSS_URL,
+    }
+))
 
 module.exports = router
